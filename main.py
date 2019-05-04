363c9475b40f9b414d8831bd3f1f861c99e5ca44
Julio Scherer <DESTRUCTIONGOATY@Julios-MacBook-Air.local>
2019-05-04 12:40:52 -0500

    initial game

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+size = width, height = 500, 288
+
+screen = pygame.display.set_mode(size)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+	screen.fill(0)
+	pygame.display.flip()
